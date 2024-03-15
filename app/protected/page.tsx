098620539b9d5592cb3e06985d30cd2bc9aa3b2d
vinicdev <vinic8670@gmail.com>
2024-03-15 11:54:22 -0300

    add: modificando pagina principal

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,9 +1,8 @@
-import DeployButton from "@/components/DeployButton";
 import AuthButton from "@/components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
-import FetchDataSteps from "@/components/tutorial/FetchDataSteps";
-import Header from "@/components/Header";
 import { redirect } from "next/navigation";
+import Notes from "@/components/Notes";
+import RegisterNotes from "@/components/RegisterNote";
 
 export default async function ProtectedPage() {
   const supabase = createClient();
@@ -19,36 +18,32 @@ export default async function ProtectedPage() {
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <div className="w-full">
-        <div className="py-6 font-bold bg-purple-950 text-center">
-          This is a protected page that you can only see as an authenticated
-          user
-        </div>
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-            <DeployButton />
+          <div className="w-full max-w-4xl flex justify-end items-center p-3 text-sm">
             <AuthButton />
           </div>
         </nav>
       </div>
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <Header />
+        <header>
+          <RegisterNotes userId={user.id} />
+        </header>
         <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          <FetchDataSteps />
+          <Notes userId={user.id} />
         </main>
       </div>
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
-          Powered by{" "}
+          Feito por{" "}
           <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+            href="https://viniciuscardoso.dev"
             target="_blank"
             className="font-bold hover:underline"
             rel="noreferrer"
           >
-            Supabase
+            Vinicius Cardoso
           </a>
         </p>
       </footer>
