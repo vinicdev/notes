c066326da6fba261b278c16b2cb2453c9b937802
vinicdev <vinic8670@gmail.com>
2024-03-15 00:16:30 -0300

    add: modificando botão de sair

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -19,10 +19,10 @@ export default async function AuthButton() {
 
   return user ? (
     <div className="flex items-center gap-4">
-      Hey, {user.email}!
+      Olá, {user.email}!
       <form action={signOut}>
         <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
-          Logout
+          Sair
         </button>
       </form>
     </div>
